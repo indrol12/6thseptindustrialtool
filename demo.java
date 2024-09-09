53e391511181e8030f0844bba2d02841cd623877
Pratiksha Anil Indrol <164622987+indrol12@users.noreply.github.com>
2024-09-09 11:27:16 +0530

    Create demo.java
    addition program

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo{
+public static void main(String args[]){
+System.out.println("hello Java");
+int a,b,result;
+result=a+b;
+System.out.println("Addition is: "+result);
+}
+}
+
+
+
